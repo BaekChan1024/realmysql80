84109c22320e6a9c68cdcddd9ae7fa4080c16ba4
baekchan1024 <baekchan1024@gmail.com>
2024-10-15 20:44:32 +0900

    initialize project

---
+++ src/main/java/study/realmysql80/Realmysql80Application.java
@@ -0,0 +1,13 @@
+package study.realmysql80;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Realmysql80Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Realmysql80Application.class, args);
+    }
+
+}
---
+++ src/test/java/study/realmysql80/Realmysql80ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.realmysql80;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Realmysql80ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
